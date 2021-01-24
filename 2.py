1914106da5e69d203ff73dff68ae8b45d7d55b26
acvc279 <77939452+acvc279@users.noreply.github.com>
2021-01-24 16:02:59 -0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+# Deleting two characters and reversing a string
+a = input("Enter a string:")
+a = a[-3::-1]
+print(a)
+# Basic Arithematic operations
+N1 = int(input("\nEnter a number:"))
+print("N1 =", N1)
+N2 = int(input("Enter a anther number:"))
+print("N2 =", N2)
+print("\n%d + %d =" % (N1, N2), N1 + N2)
+print("%d - %d =" % (N1, N2), N1 - N2)
+print("%d * %d =" % (N1, N2), N1 * N2)
+print("%d / %d =" % (N1, N2), N1 / N2)
